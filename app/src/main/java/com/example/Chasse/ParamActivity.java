1e0885913a405cae23de7a6bfd8ce6a37a596108
Dewnyl <nathan.bernard2804@gmail.com>
2024-10-22 12:04:45 +0200

    modification profile

--- app/src/main/java/com/example/Chasse/ParamActivity.java
+++ app/src/main/java/com/example/Chasse/ParamActivity.java
@@ -1,4 +1,6 @@
 package com.example.Chasse;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,21 +21,14 @@ public class ParamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_main);
-        /*
         modifprofil = (Button) findViewById(R.id.modifprofilbtn);
 
-        modifprofil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                PopupMenu popup = new PopupMenu(ParamActivity.this,modifprofil);
-
-                popup.getMenuInflater().inflate(R.menu.popup_setting_profil,popup.getMenu());
-                popup.show();
-            }
+        this.modifprofil.setOnClickListener(v -> {
+            Intent intent = new Intent(ParamActivity.this, ChangeProfilActivity.class);
+            startActivity(intent);
         });
 
-         */
+
 
     }
 
