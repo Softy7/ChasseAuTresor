84c60b24d92dbbd8aba1083bd51005cefab6c1b9
nathan <nathan.bernard2804@gmail.com>
2024-10-22 18:13:38 +0200

    réglage problème

--- app/src/main/java/com/example/Chasse/ParamActivity.java
+++ app/src/main/java/com/example/Chasse/ParamActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
