bbbdbc67f209146912046a479e08651e98db2364
Softy7 <ewanrecquignies@gmail.com>
2024-10-23 11:14:46 +0200

    Accès au choix du thème sur la création de la partie (recherche d'utilisateur non fonctionnelle !!!!!)

--- app/src/main/java/com/example/Chasse/InviteFriendActivity.java
+++ app/src/main/java/com/example/Chasse/InviteFriendActivity.java
@@ -1,6 +1,11 @@
 package com.example.Chasse;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,10 +16,49 @@ public class InviteFriendActivity extends AppCompatActivity {
 
     private MainSystem mainSystem = new MainSystem();
     public Game game = new Game();
+    protected TextView code, theme;
+    protected ImageButton back, launch, search;
+    protected int idTheme;
+
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
+        setContentView(R.layout.add_friend_game);
+        this.idTheme = getIntent().getIntExtra("idTheme", 0);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         this.game.addUser(mainSystem.readUser(this));
         this.game.setCode();
+
+        this.code = findViewById(R.id.code);
+        CharSequence join = "Code: "+this.game.getCode();
+        this.code.setText(join);
+
+        this.theme = findViewById(R.id.theme);
+        this.theme.setText(this.getTheTheme());
+
+        this.back = findViewById(R.id.back);
+        this.back.setOnClickListener(v -> finish());
+
+        this.launch = findViewById(R.id.launch);
+        this.launch.setOnClickListener(v -> {});
+
+        this.search = findViewById(R.id.search);
+        this.search.setOnClickListener(v -> {});
+
+    }
+
+    private String getTheTheme() {
+        return switch (this.idTheme) {
+            default -> "Les Pirates";
+            case 1 -> "Les Alcools";
+            case 2 -> "L'IUT";
+            case 3 -> "La Nature";
+            case 4 -> "Les voitures";
+            case 5 -> "L'informatique";
+        };
     }
 }
