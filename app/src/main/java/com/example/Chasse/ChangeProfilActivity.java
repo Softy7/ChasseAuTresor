6339a7762fb79744b0539975d2a91ba278f11e35
Dewnyl <nathan.bernard2804@gmail.com>
2024-10-24 11:26:35 +0200

    ajout des retours + infos utilisateur affiché pour les setting

--- app/src/main/java/com/example/Chasse/ChangeProfilActivity.java
+++ app/src/main/java/com/example/Chasse/ChangeProfilActivity.java
@@ -1,15 +1,53 @@
 package com.example.Chasse;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.Chasse.Model.System.MainSystem;
+import com.example.Chasse.Model.User;
+
 public class ChangeProfilActivity extends AppCompatActivity {
+
+    protected ImageButton backsetting;
+
+    @SuppressLint({"SetTextI18n","MissingInflatedId", "LocalSuppress"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_profil);
+
+        //Affichage nom prenom à la place de "info bdd"
+        User user = new MainSystem().readUser(this);
+        String nom = user.getFirstName();
+        String prenom = user.getLastName();
+        String pseudo = user.getPseudo();
+        TextView nomprenom=findViewById(R.id.nameaffiche);
+        nomprenom.setText(nom + " " + prenom);
+        TextView pseudoaffiche=findViewById(R.id.pseudo);
+        pseudoaffiche.setText(pseudo);
+        TextView nomaffiche=findViewById(R.id.nom);
+        nomaffiche.setText(nom);
+        TextView prenomaffiche=findViewById(R.id.prenom);
+        prenomaffiche.setText(prenom);
+
+
+
+        this.backsetting = findViewById(R.id.backsetting);
+        this.backsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChangeProfilActivity.this,ParamActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/Chasse/ParamActivity.java
+++ app/src/main/java/com/example/Chasse/ParamActivity.java
@@ -1,14 +1,21 @@
 package com.example.Chasse;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.viewpager.widget.ViewPager;
+
+import com.example.Chasse.Model.System.MainSystem;
+import com.example.Chasse.Model.User;
 import com.google.android.material.tabs.TabLayout;
 
 
@@ -16,10 +23,20 @@ public class ParamActivity extends AppCompatActivity {
 
     protected AppCompatButton modifprofil;
 
+    protected ImageButton backsetting;
+
+    @SuppressLint({"SetTextI18n", "MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_main);
+
+        //Affichage nom prenom à la place de "info bdd"
+        User user = new MainSystem().readUser(this);
+        String nom = user.getFirstName();
+        String prenom = user.getLastName();
+        TextView nomprenom=findViewById(R.id.nameaffiche);
+        nomprenom.setText(nom + " " + prenom);
 
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN |
@@ -34,5 +51,17 @@ public class ParamActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        this.backsetting = findViewById(R.id.backsetting);
+        this.backsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ParamActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
