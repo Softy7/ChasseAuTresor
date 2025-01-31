ae64dd670eaf9ba3cfb4a50c0dbda66df923908a
valmenu59 <valentin.menu@outlook.com>
2025-01-31 15:39:32 +0100

    Ajout de tests

--- app/src/main/java/com/example/Chasse/Activities/ChoiceConnectActivity.java
+++ app/src/main/java/com/example/Chasse/Activities/ChoiceConnectActivity.java
@@ -2,6 +2,7 @@ package com.example.Chasse.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -37,22 +38,28 @@ public class ChoiceConnectActivity extends AppCompatActivity {
 
         bateau = findViewById(R.id.bateau);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.bateau);
-        bateau.startAnimation(animation);
 
         nuage_1 = findViewById(R.id.nuage_1);
         Animation animation1 = AnimationUtils.loadAnimation(this,R.anim.nuage_1);
         animation1.setInterpolator(new LinearInterpolator());
-        nuage_1.startAnimation(animation1);
 
         nuage_2 = findViewById(R.id.nuage_2);
         Animation animation2 = AnimationUtils.loadAnimation(this,R.anim.nuage_2);
         animation2.setInterpolator(new LinearInterpolator());
-        nuage_2.startAnimation(animation2);
 
         nuage_3 = findViewById(R.id.nuage_3);
         Animation animation3 = AnimationUtils.loadAnimation(this,R.anim.nuage_3);
         animation3.setInterpolator(new LinearInterpolator());
-        nuage_3.startAnimation(animation3);
+
+
+        Log.d("Test", String.valueOf(getIntent().getBooleanExtra("test_mode",false)));
+        if (!getIntent().getBooleanExtra("test_mode", false)) {
+            bateau.startAnimation(animation);
+            nuage_1.startAnimation(animation1);
+            nuage_2.startAnimation(animation2);
+            nuage_3.startAnimation(animation3);
+        }
+
 
         if (mainSystem.readUser(ChoiceConnectActivity.this) != null) {
             finish();
@@ -62,14 +69,18 @@ public class ChoiceConnectActivity extends AppCompatActivity {
         this.connect.setOnClickListener(v -> {
             Intent intent = new Intent(ChoiceConnectActivity.this, LoginActivity.class);
             startActivity(intent);
-            finish();
+            if (!getIntent().getBooleanExtra("test_mode", false)) {
+                finish();
+            }
         });
 
         this.registrer = findViewById(R.id.registrering);
         this.registrer.setOnClickListener(v -> {
             Intent intent = new Intent(ChoiceConnectActivity.this, Registractivity.class);
             startActivity(intent);
-            finish();
+            if (!getIntent().getBooleanExtra("test_mode", false)) {
+                finish();
+            }
         });
 
 
